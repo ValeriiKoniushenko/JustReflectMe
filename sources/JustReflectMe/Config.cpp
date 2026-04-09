5f72508cfa837ee83439441fcde5758156b3655e
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2026-04-09 22:27:28 +0200

    refactor(Config): handle empty values for insertCodeAtTheTop and insertCodeAtTheBottom
    - Updated logic to skip adding quotes for empty `insertCodeAtTheTop` and `insertCodeAtTheBottom` values in config output generation.

--- sources/JustReflectMe/Config.cpp
+++ sources/JustReflectMe/Config.cpp
@@ -201,11 +201,17 @@ namespace JRM
         out += genKey(*config.alwaysDirtyCache)
                + (config.alwaysDirtyCache->value ? "true" : "false") + "\n\n"s;
 
-        out += genKey(*config.insertCodeAtTheTop) + "\"" + config.insertCodeAtTheTop->value
-               + "\"\n\n"s;
+        out += genKey(*config.insertCodeAtTheTop)
+               + (config.insertCodeAtTheTop->value.empty()
+                      ? ""
+                      : ("\"" + config.insertCodeAtTheTop->value + "\""))
+               + "\n\n"s;
 
-        out += genKey(*config.insertCodeAtTheBottom) + "\"" + config.insertCodeAtTheBottom->value
-               + "\"\n\n"s;
+        out += genKey(*config.insertCodeAtTheBottom)
+               + (config.insertCodeAtTheBottom->value.empty()
+                      ? ""
+                      : ("\"" + config.insertCodeAtTheBottom->value + "\""))
+               + "\n\n"s;
 
         return out;
     }
